01a154faef4820226349889fd9d9434428e03165
Philippe Nizer <pnizer@gmail.com>
2020-05-31 10:21:34 -0300

    First commit

---
+++ .zshrc
@@ -0,0 +1,55 @@
+# Created by newuser for 5.4.2
+
+autoload -U compinit; compinit
+autoload -U promptinit; promptinit
+
+prompt pure
+
+export PATH=/home/linuxbrew/.linuxbrew/bin:/usr/local/bin:/home/nizer/.local/bin:$PATH
+
+source /home/linuxbrew/.linuxbrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# colored GCC warnings and errors
+#export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+zstyle ':completion:*' menu select
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+HISTSIZE=10000
+SAVEHIST=10000
+HISTFILE=~/.zsh_history
+
+setopt APPEND_HISTORY
+setopt share_history
+
+export DOCKER_HOST=localhost:2375
+
+export PURE_PROMPT_SYMBOL=">"
+export PURE_PROMPT_VICMD_SYMBOL="<"
+export PURE_GIT_DOWN_ARROW="↓"
+export PURE_GIT_UP_ARROW="↑"
+
+source ~/.zsh/plugins/zsh-term-title/term-title.plugin.zsh
+
+unsetopt BEEP
+
+source <(kubectl completion zsh)
+source /home/nizer/.local/bin/aws_zsh_completer.sh
+source ~/.zsh/completion/_docker
---
+++ customize.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+sudo apt-get update
+sudo apt-get install build-essential -y
+
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+brew install node
+
+# zsh
+sudo apt-get install zsh -y
+
+sudo usermod -a -G staff nizer
+npm install --global pure-prompt
+
+brew install zsh-syntax-highlighting
+
+git clone https://github.com/pawel-slowik/zsh-term-title ~/.zsh/plugins/zsh-term-title
+
+cp .zshrc ~
+cp .dircolors ~
+
+chsh -s /bin/zsh
+
+# tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+cp .tmux ~
+
+# docker
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+
+mkdir -p ~/.zsh/completion
+curl -fLo ~/.zsh/completion/_docker https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
+
+# kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# python3
+sudo apt-get install python3-pip -y
+
+sudo apt-get install ffmpeg -y
+pip3 install youtube-dl 
+pip3 install tinydb
+
+# python2
+sudo apt-get install python python-pip -y
+
+# envy code r
+mkdir fonts
+cd fonts
+wget https://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip
+unzip EnvyCodeR-PR7.zip
+git clone https://github.com/powerline/fontpatcher.git
+sudo apt-get install fontforge -y
+
+
+# awscli
+pip3 install awscli
+
